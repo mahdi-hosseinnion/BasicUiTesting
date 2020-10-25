5af94cba0b77173a4dc2bd9de5d8ad1944681112
seyed mahdi <ssmmhh1382@gmail.com>
2020-10-25 12:33:41 +0100

    03- Activity navigation espresso

--- app/src/androidTest/java/com/example/basicuitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/basicuitesting/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.example.basicuitesting
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -21,17 +23,16 @@ class MainActivityTest {
     }
 
     @Test
-    fun testButtonAndTextView_inView() {
+    fun test_navToSecondaryActivity() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //mainActivity
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_next_activity)).perform(click())
+        //secondary activity
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun testTextView_text() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
+        pressBack()
+        //mainActivity
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
