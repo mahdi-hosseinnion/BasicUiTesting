baa70838fe10f65f80b4f00e06ebaac488c72873
seyed mahdi <ssmmhh1382@gmail.com>
2020-10-28 11:43:32 +0100

    10-Dialog input testing (Material Dialogs)

--- app/src/androidTest/java/com/example/basicuitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/basicuitesting/MainActivityTest.kt
@@ -6,13 +6,16 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.basicuitesting.ui.ImageViewHasDrawableMatcher.hasDrawable
 import com.example.basicuitesting.ui.movie.KEY_IMAGE_DATA
 import com.example.basicuitesting.ui.movie.MainActivity
@@ -24,36 +27,26 @@ import org.junit.Test
 //@RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
-    @get:Rule
-    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
-
     @Test
     fun test_validateIntentSentToPickPackage() {
         //GIVEN
-        val expectedIntent: Matcher<Intent> =
-            hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
-        val activityResult = createGalleryPickActivityResultStub()
-        intending(expectedIntent).respondWith(activityResult)
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val expectedValue = "HAHA MY NAME IS ROBOT I WILL KILL YOU HAHAHAHAHAHA LOL MY NAME IS ROBOT"
 
         //execute and verify
-        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
-        onView(withId(R.id.button_open_camera)).perform(click())
-        intending(expectedIntent)
-        onView(withId(R.id.image)).check(matches(hasDrawable()))
+        onView(withId(R.id.button_launch_dialog)).perform(click())
+        onView(withText(R.string.text_enter_you_name)).check(matches(isDisplayed()))
+        onView(withText(R.string.ok)).perform(click())
+        onView(withText(R.string.text_enter_you_name)).check(matches(isDisplayed()))
+
+        //enter some input
+        onView(withId(R.id.md_input_message)).perform(typeText(expectedValue))
+        onView(withText(R.string.ok)).perform(click())
+        //make sure dialog gone
+        onView(withText(R.string.text_enter_you_name)).check(doesNotExist())
+        //confirm name is set to textView in activity
+        onView(withId(R.id.text_name)).check(matches(withText(expectedValue)))
     }
 
-    private fun createGalleryPickActivityResultStub(): ActivityResult {
-        val bundle = Bundle()
-        bundle.putParcelable(
-            KEY_IMAGE_DATA,
-            BitmapFactory.decodeResource(
-                intentsTestRule.activity.resources,
-                R.drawable.ic_launcher_background
-            )
-        )
-        val resultIntent = Intent()
-        resultIntent.putExtras(bundle)
-        return ActivityResult(RESULT_OK, resultIntent)
-    }
+
 }
--- app/src/main/java/com/example/basicuitesting/ui/movie/MainActivity.kt
+++ app/src/main/java/com/example/basicuitesting/ui/movie/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.bumptech.glide.Glide
 import com.example.basicuitesting.R
 import com.example.basicuitesting.factory.MovieFragmentFactory
@@ -15,38 +17,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 const val REQUEST_IMAGE_CAPTURE = 6742
 private const val TAG = "MainActivity"
 const val KEY_IMAGE_DATA = "data"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button_open_camera.setOnClickListener {
-            dispatchCameraIntent()
+        button_launch_dialog.setOnClickListener {
+            showDialog()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            Log.d(TAG, "onActivityResult: RESULT OK")
-            when (requestCode) {
-                REQUEST_IMAGE_CAPTURE -> {
-                    Log.d(TAG, "onActivityResult: testing01 called")
-                    data?.extras.let { extras ->
-                        if (extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
-                            Log.d(TAG, "onActivityResult: testing01 called didint path")
-                            return
-                        }
-                        Log.d(TAG, "onActivityResult: testing01 called path")
-
-                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
-                        image.setImageBitmap(imageBitmap)
-                    }
+    private fun showDialog() {
+        MaterialDialog(this)
+            .show {
+                input(
+                    hint = "mahdi...",
+                    waitForPositiveButton = true,
+                    allowEmpty = false
+                ) { dialog, name ->
+                    setNameToTextView(name.toString())
                 }
+                title(R.string.text_enter_you_name)
+                positiveButton(R.string.ok)
             }
-        }
+
     }
-    private fun dispatchCameraIntent() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+    private fun setNameToTextView(name: String) {
+        text_name.text = name
     }
 }
